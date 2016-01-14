5cbd1ffd15abbfa41a324fd4fa59476695202dc3
kyly1982 <anloy_3633@sohu.com>
2016-01-14 09:25:44 +0800

    添加支持旋转，缩放和拖曳的图片

--- app/src/main/java/com/example/kyly/demo/MainActivity.java
+++ app/src/main/java/com/example/kyly/demo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.kyly.demo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
@@ -16,6 +19,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import widget.StickerView.StickerView;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,View.OnTouchListener,GestureDetector.OnGestureListener {
     private GestureDetector mGestureDetector;
 
@@ -26,9 +31,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private AppCompatButton addBackground;
     private AppCompatImageView background;
     private RelativeLayout root;
-    private Button resize;
+    private Button resizeButton;
+    private TextView point;
 
-    private View view;
+    private View mCurrentImage;
 
     private int screenWidth;
     private int screenHeight;
@@ -38,10 +44,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int dx;
     private int dy;
 
-    private int width = 60;
+    private int width = 100;
+    private int imageWidth=400;
+    private int imageHeight=300;
 
     private boolean isMoving = false;
 
+
+    //当前的的帖纸
+    private StickerView mCurrentView;
+
+    //存储贴纸列表
+    private ArrayList<View> mStickers = new ArrayList<>();
 
 
     @Override
@@ -72,15 +86,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         addBackground = (AppCompatButton) findViewById(R.id.addbackground);
         background = (AppCompatImageView) findViewById(R.id.background);
         root = (RelativeLayout) findViewById(R.id.rootlayout);
-        resize = new Button(this);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(width,width);
-        resize.setLayoutParams(params);
-        resize.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-        resize.setVisibility(View.GONE);
-        resize.setOnTouchListener(this);
-        root.addView(resize);
+        point = (TextView) findViewById(R.id.point);
 
-
+        root.setOnClickListener(this);
         addPics.setOnClickListener(this);
         addTips.setOnClickListener(this);
         addBackground.setOnClickListener(this);
@@ -97,9 +105,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 addNewTip();
                 break;
             case R.id.addpic:
-                addImage();
+                addStickerView();
                 break;
             case R.id.addbackground:
+                if (null != mCurrentView){
+                    final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+                    mCurrentView.setBitmapBackground(bitmap);
+                }
+                break;
+            case R.id.rootlayout:
+                if (null != resizeButton) {
+                    resizeButton.setVisibility(View.GONE);
+                }
                 break;
         }
     }
@@ -117,25 +134,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tips.add(textView);
         root.addView(textView);
         textView.postInvalidate();
+
+        if (null != resizeButton){
+            resizeButton.setVisibility(View.GONE);
+        }
     }
 
     private void addImage(){
         RelativeLayout view = (RelativeLayout) LayoutInflater.from(this).inflate(R.layout.image_layout,null);
-       /* RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(300,200);
-        params.setMargins(screenWidth / 2 - 150,screenHeight / 2 -100,screenWidth / 2 + 150,screenHeight / 2 + 100);
-        view.setLayoutParams(params);*/
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(imageWidth,imageHeight);
+        params.setMargins((screenWidth - imageWidth) / 2, (screenHeight - imageHeight) / 2, (screenWidth - imageWidth) / 2, (screenHeight - imageHeight) / 2);
+        view.setLayoutParams(params);
         view.setOnTouchListener(this);
-        //view.setVisibility(View.GONE);
-        root.addView(view);
-        view.invalidate();
-
-        view.layout(screenWidth / 2 - 150,screenHeight / 2 -100,screenWidth / 2 + 150,screenHeight / 2 + 100);
-        resize.layout(view.getWidth() - width, view.getTop(), view.getRight(), view.getTop() + width);
-        resize.setVisibility(View.VISIBLE);
         images.add(view);
-
-        this.view = view;
+        root.addView(view);
+        this.mCurrentImage = view;
+        if (null == resizeButton){
+            createResizeButton();
+        } else {
+           showResizeButton();
+        }
     }
+
+    //添加帖纸
+    private void addStickerView() {
+        final StickerView stickerView = new StickerView(this);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        final Bitmap bgBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.bg_login_guide);
+        stickerView.setWaterMark(bitmap, bgBitmap);
+        root.addView(stickerView, lp);
+        mStickers.add(stickerView);
+        mCurrentView = stickerView;
+        //setCurrentEdit(stickerView);
+    }
+
+
+    private void createResizeButton(){
+        resizeButton = new Button(this);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(width,width);
+        params.setMargins((screenWidth + imageWidth) / 2 - width, (screenHeight - imageHeight) / 2 - width, (screenWidth - imageWidth) / 2, (screenHeight + imageHeight) / 2);
+        resizeButton.setLayoutParams(params);
+        resizeButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        resizeButton.setOnTouchListener(this);
+        root.addView(resizeButton);
+    }
+
+    private void showResizeButton(){
+        resizeButton.bringToFront();
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) resizeButton.getLayoutParams();
+        params.setMargins(mCurrentImage.getLeft() + mCurrentImage.getWidth() - width,mCurrentImage.getTop() - width,screenWidth - mCurrentImage.getRight(),mCurrentImage.getTop());
+        resizeButton.setLayoutParams(params);
+        resizeButton.setVisibility(View.VISIBLE);
+    }
+
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
@@ -145,26 +197,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.e("screen",screenWidth + ","+screenHeight );
         }
 
-        int left,right,top,bottom;
-
         int action = event.getAction();
         switch (action){
             case MotionEvent.ACTION_DOWN:
                 lastX = (int) event.getRawX();
                 lastY = (int) event.getRawY();
-                Log.e("","width="+v.getWidth() +",height="+v.getHeight()+",x="+event.getX()+",y="+event.getY());
+
                 if (! (v instanceof Button)){
-                    view = v;
+                    mCurrentImage = v;
+
                 }
+
+
                 break;
             case MotionEvent.ACTION_MOVE:
-                dx = (int)event.getRawX() - lastX;
+                dx =  (int)event.getRawX() - lastX;
                 dy = (int)event.getRawY() - lastY;
 
-                if(!isMoving && (Math.abs(dx) > 3 || Math.abs(dy) > 3)){
+                if(!isMoving && (Math.abs(dx) + Math.abs(dy) > 3)){
                     isMoving = true;
                 }
-
                 if (v instanceof Button){
                     resizeImage(dx,dy);
                 } else {
@@ -172,23 +224,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
 
 
-
                 lastX = (int) event.getRawX();
                 lastY = (int) event.getRawY();
                 break;
             case MotionEvent.ACTION_UP:
-                left = v.getLeft();
-                top = v.getTop();
-                right = screenWidth - left - v.getWidth();
-                bottom = screenHeight - top - v.getHeight();
+                int left = v.getLeft();
+                int top = v.getTop();
+                int right = screenWidth - left - v.getWidth();
+                int bottom = screenHeight - top - v.getHeight();
 
                 RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) v.getLayoutParams();
                 params.setMargins(left, top, right, bottom);
+                v.setLayoutParams(params);
                 if (v instanceof RelativeLayout ) {
-                    resize.layout(v.getLeft() + v.getWidth() - width, v.getTop(), v.getRight(), v.getTop() + width);
-                    params = (RelativeLayout.LayoutParams) resize.getLayoutParams();
-                    params.setMargins(resize.getLeft(),resize.getTop(),screenWidth - resize.getLeft() - width,screenHeight - resize.getTop() - width);
-                    resize.setLayoutParams(params);
+                    resizeButton.layout(v.getLeft() + v.getWidth() - width, v.getTop() - width, screenWidth - v.getRight(), screenHeight -v.getTop());
+                    params = (RelativeLayout.LayoutParams) resizeButton.getLayoutParams();
+                    params.setMargins(resizeButton.getLeft(),resizeButton.getTop(),screenWidth - resizeButton.getLeft() - width,screenHeight - resizeButton.getTop() - width);
+                    resizeButton.setLayoutParams(params);
                 }
                 isMoving = false;
                 break;
@@ -199,42 +251,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void resizeImage(int dx,int dy){
 
+        Log.e("", "11111111111111111");
+        moveView(resizeButton,dx,dy);
+        int width = mCurrentImage.getWidth() + dx;
+        int height = mCurrentImage.getHeight() - dy;
+
+
+
+        int left = mCurrentImage.getLeft();
+        int bottom = screenHeight - mCurrentImage.getBottom();
+        int top = mCurrentImage.getBottom() - height;
+        int right = screenWidth  - mCurrentImage.getLeft() - width;
+
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(width,height);
+        layoutParams.setMargins(left, right, top, bottom);
+        mCurrentImage.setLayoutParams(layoutParams);
+        //mCurrentImage.layout(mCurrentImage.getLeft(),mCurrentImage.getTop() + dy,mCurrentImage.getRight() + dx,mCurrentImage.getBottom());
     }
 
     private void moveView(View v,int dx,int dy){
-        int left,right,top,bottom;
-        left = v.getLeft() + dx;
-        right = v.getRight() + dx;
-        top = v.getTop() + dy;
-        bottom = v.getBottom() + dy;
+        //获取移动后的边距
+        int left = v.getLeft() + dx;
+        int top = v.getTop() + dy;
+        int right = v.getRight() + dx;
+        int bottom = v.getBottom() + dy;
 
-        if (left <= 0){
+        //修正坐标点
+        if (left < 0){
             left = 0;
             right =  v.getWidth();
             Log.e("warn","left < 0");
         }
 
-        if (left >= screenWidth - v.getWidth()){
+        if (right > screenWidth){
             left = screenWidth - v.getWidth();
             right = screenWidth;
             Log.e("warn","right < 0");
         }
 
-        if(top <= 0){
+        if(top < 0){
             top = 0;
             bottom = v.getHeight();
             Log.e("warn", "top < 0");
         }
 
-        if (top >= screenHeight - v.getHeight()){
+        if (bottom > screenHeight){
             bottom = screenHeight;
             top = screenHeight - v.getHeight();
             Log.e("warn","bottom < 0");
         }
+        point.setText(getResources().getString(R.string.point, left, top, right, bottom, dx, dy));
 
-        v.layout(left, top, right,bottom);
-        if (v instanceof RelativeLayout ) {
-            resize.layout(v.getLeft() + v.getWidth() - width, v.getTop(), v.getRight(), v.getTop() + width);
+
+//        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) v.getLayoutParams();
+//        params.setMargins(left,top,right,bottom);
+//        v.setLayoutParams(params);
+        v.layout(left, top, right, bottom);
+        if (v instanceof RelativeLayout && null != resizeButton) {
+            Log.e("","22222222222222");
+            resizeButton.layout(v.getLeft() + v.getWidth() - width, v.getTop() - width, v.getRight(), v.getTop());
         }
 
         if (v instanceof Button){
@@ -255,6 +331,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
+        if (mCurrentImage instanceof RelativeLayout) {
+            showResizeButton();
+        } else {
+            if (null != resizeButton) {
+                resizeButton.setVisibility(View.GONE);
+            }
+        }
         return true;
     }
 
@@ -267,14 +350,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onLongPress(MotionEvent e) {
         Log.e("","isMoving="+isMoving);
         if(!isMoving) {
-            if (view instanceof TextView) {
-                tips.remove(view);
+            if (mCurrentImage instanceof TextView) {
+                tips.remove(mCurrentImage);
             } else {
-                images.remove(view);
-            }
+                images.remove(mCurrentImage);
 
-            view.setVisibility(View.GONE);
-            view = null;
+            }
+            mCurrentImage.setVisibility(View.GONE);
+            root.removeView(mCurrentImage);
+            root.removeView(resizeButton);
+            resizeButton = null;
+            mCurrentImage = null;
         }
     }
 
@@ -282,5 +368,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         return true;
     }
+
+
+    /**
+     * 设置当前处于编辑模式的贴纸
+     */
+    private void setCurrentEdit(StickerView stickerView) {
+        if (mCurrentView != null) {
+            //mCurrentView.setInEdit(false);
+        }
+        mCurrentView = stickerView;
+        //stickerView.setInEdit(true);
+    }
+
+    private void generateBitmap() {
+
+        Bitmap bitmap = Bitmap.createBitmap(root.getWidth(),
+                root.getHeight()
+                , Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        root.draw(canvas);
+
+        /*String iamgePath = FileUtils.saveBitmapToLocal(bitmap, this);
+        Intent intent = new Intent(this, DisplayActivity.class);
+        intent.putExtra("image", iamgePath);
+        startActivity(intent);*/
+    }
+
 
 }
